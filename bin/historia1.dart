cfc517636e1d2cbf86654b02b161a54468ca196e
catalunha <catalunha.mj@gmail.com>
2023-12-05 11:58:15 -0300

    rev

--- bin/historia1.dart
+++ bin/historia1.dart
@@ -1,14 +1,26 @@
 import 'dart:io';
 
-void main() {
-  print(
+void main() async {
+  await meuPrint(
       'Você acorda de manhã e seu objetivo é ir pro seu trabalho. Qual a primeira coisa que você faz?');
-  print('1 Escovo os dentes.');
-  print('2 Lavo meu rosto');
-  print('3 Tomo um banho');
-  print('(Digite um número correspondente a sua escolha).');
-  String? digitado = stdin.readLineSync();
-  if (digitado == '1') {
-    print('');
+  await Future.delayed(Duration(seconds: 1));
+  await meuPrint('1 Escovo os dentes.');
+  await Future.delayed(Duration(seconds: 1));
+  await meuPrint('2 Lavo meu rosto');
+  await Future.delayed(Duration(seconds: 1));
+  await meuPrint('3 Tomo um banho');
+  await Future.delayed(Duration(seconds: 1));
+  // String? digitado = stdin.readLineSync();
+  // if (digitado == '1') {
+  //   print('');
+  // }
+}
+
+Future<void> meuPrint(String texto) async {
+  List<String> letras = texto.split('');
+  for (var letra in letras) {
+    await Future.delayed(Duration(milliseconds: 100));
+    stdout.write(letra);
   }
+  stdout.write('\n');
 }
